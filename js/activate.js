bacce51ccad04ec27a226a996a5e3be6dbd4f6ff
jobayer637 <web.jobayer@gmail.com>
2020-09-18 16:20:17 +0600

    LEDP Assignment 2, using bootstrap, html, css, jq, link: http://lambda.oxygenna.com/corporate

---
+++ js/activate.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: 400,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 4
+        },
+        1280: {
+            items: 5
+        },
+        1440: {
+            items: 6
+        },
+        1500: {
+            items: 7
+        }
+    }
+})
